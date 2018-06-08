7a9b5cfcb0eaef51ecb18ca0f65b8bc1588eb523
kingly <libintm@163.com>
2018-06-08 09:34:43 +0800

    优化

--- Example/KYContact/KYViewController.m
+++ Example/KYContact/KYViewController.m
@@ -26,4 +26,25 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+
+- (IBAction)addContact:(id)sender
+{
+  UIButton * sendButton = (UIButton *)sender;
+  
+  //button不能再点击
+  sendButton.enabled = false;
+  
+ // [self.contactManager addContact:[RITLContactObject testContactObject]];
+  
+  //3s后才可再添加,为了能够承接3秒变化通知的响应
+  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    
+    [sendButton setEnabled:true];
+    
+  });
+  
+}
+
+
 @end
